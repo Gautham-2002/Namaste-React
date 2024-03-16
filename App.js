274fe47e170ecac891c6044bde9a53875e8314d0
gautham <gautham.gokulakonda@apxor.com>
2024-03-16 20:59:31 +0530

    feat: :sparkles: React Component

--- App.js
+++ App.js
@@ -2,13 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 // React Element
-const jsxHeading = <h1 id="heading">Namste React using jsx</h1>;
+const jsxHeading = <h1 id="heading">Namste React using jsx 🚀 </h1>;
 
-//React Component
+const JsxHeading2 = () => <h1 id="heading">Namste React using jsx 🚀 </h1>;
+
+//Component composition - a component inside another component
+
+//React Component   - a js function which returns jsx/react element
 const HeadingComponent = () => (
   <div className="container">
+    <JsxHeading2 />
     <h1 className="Heading">Namaste React Functional Component</h1>
   </div>
 );
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+root.render(<HeadingComponent />);
