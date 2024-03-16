168fa25f7a424e1e4f55e0978f3c492c2d6646b8
gautham <gautham.gokulakonda@apxor.com>
2024-03-16 20:52:32 +0530

    feat: :sparkles: React Element

--- App.js
+++ App.js
@@ -1,22 +1,14 @@
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading" },
-//   "Hello world from React"
-// );
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
-  ])
-);
-console.log(parent);
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// React Element
+const jsxHeading = <h1 id="heading">Namste React using jsx</h1>;
 
-// root.render(heading);
-root.render(parent);
+//React Component
+const HeadingComponent = () => (
+  <div className="container">
+    <h1 className="Heading">Namaste React Functional Component</h1>
+  </div>
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(jsxHeading);
